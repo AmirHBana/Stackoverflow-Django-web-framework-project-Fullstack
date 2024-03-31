6b330b2c5aaf11023222ae110e3078e75aa72417
AmirHBana <amir666arrow@gmail.com>
2024-03-31 23:58:10 +0330

    push code to github

---
+++ Stackoverflow_django_project/stackusers/apps.py
@@ -0,0 +1,13 @@
+from django.apps import AppConfig
+
+
+class StackusersConfig(AppConfig):
+
+    default_auto_field = 'django.db.models.BigAutoField'
+
+    name = 'stackusers'
+
+
+    def ready(self):
+        
+        import stackusers.signals
